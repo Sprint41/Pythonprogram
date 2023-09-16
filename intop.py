2de7b7d07e6ce3ea47693755a969a3780aa41e22
Aditya Gupta <129592452+Sprint41@users.noreply.github.com>
2023-09-17 00:43:53 +0530

    Add files via upload

---
+++ intop.py
@@ -0,0 +1,10 @@
+a = int(5)
+b = int(4)
+c= a+b
+print('addition of a and b is:',c)
+d= a/b
+print('division of a and b is:',d)
+e= a - b
+print('substraction of a and b is:',e)
+f= a*b
+print('multiplication of a and b is:',f)
