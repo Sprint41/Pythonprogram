0b9726a4f425b8e4289485469caa60f3b30bdb1b
Aditya Gupta <129592452+Sprint41@users.noreply.github.com>
2023-09-17 01:15:21 +0530

    Add files via upload

---
+++ floatpreecision.py
@@ -0,0 +1,28 @@
+#Explore the precision of float data types by calculating the result of 1/3
+a= float(1)
+b= float(3)
+c= a/b
+print(c)
+
+#Concatenation of two string to create a new single string and print the result
+p= str('cutie')
+q= str('pie')
+r= p+q
+print(r)
+
+#Write a program that calculates and prints the length of a user-entered string
+string="ilovepythonprogramming"
+print(len(string))
+
+#Take an integer input from the user and convert it into a string. Then, print the string.
+num = int(50)
+print('type before conversion is:', type(num))
+converted_num= str(num)
+print('type after conversion is:',type(converted_num))
+
+
+#Get a numeric string from the user and convert it into an integer. Print the integer value.
+string = str(10)
+print('type before conversion is:',type(string))
+converted_string= int(string)
+print('type after conversion is:',type(converted_string))
