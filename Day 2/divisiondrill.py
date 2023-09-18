c6964c605b3009963bedb97a51f82f6b5159e297
Aditya Gupta <129592452+Sprint41@users.noreply.github.com>
2023-09-19 00:30:21 +0530

    Add files via upload

---
+++ Day 2/divisiondrill.py
@@ -0,0 +1,7 @@
+#Take two numbers as input and perform division. Print both the quotient and the remainder.
+num1= float(input("enter dividend:"))
+num2 = float(input("enter divisor:"))
+quotient= num1/num2
+remainder= num1%num2
+print("Quotient of",num1,"and",num2,"is:",quotient)
+print("Remainder of",num1,"and",num2,"is",remainder)
