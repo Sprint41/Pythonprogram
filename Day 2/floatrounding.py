b60855a22d950de819dd2ecc2a2caae9d8001727
Aditya Gupta <129592452+Sprint41@users.noreply.github.com>
2023-09-18 16:03:42 +0530

    Add files via upload

---
+++ Day 2/floatrounding.py
@@ -0,0 +1,6 @@
+a = float(input("Enter the first number:"))
+b = float(input("Enter the second number:"))
+c = a/b
+print("The result of a/b is:",c) 
+rounded_result= round(c,2)
+print("The rounded result upto 2 decimal number is:",rounded_result)
