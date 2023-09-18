78c3e18cbde4bf97246fb75fed97cba686dc3924
Aditya Gupta <129592452+Sprint41@users.noreply.github.com>
2023-09-18 14:02:11 +0530

    strtoint.py

---
+++ Day 2/strtoint.py
@@ -0,0 +1,5 @@
+#Get a numeric string from the user and convert it into an integer. Print the integer value.
+string = str(10)
+print('type before conversion is:',type(string))
+converted_string= int(string)
+print('type after conversion is:',type(converted_string))
