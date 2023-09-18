29380a2a418d065daa9711dec4f0230da836672c
Aditya Gupta <129592452+Sprint41@users.noreply.github.com>
2023-09-18 14:01:03 +0530

    inttostr.py

---
+++ Day 2/inttostr.py
@@ -0,0 +1,5 @@
+#Take an integer input from the user and convert it into a string. Then, print the string.
+num = int(50)
+print('type before conversion is:', type(num))
+converted_num= str(num)
+print('type after conversion is:',type(converted_num))
