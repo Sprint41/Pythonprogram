7eb1fec3e5276fdc626bbf423670c3c7dc54d114
Aditya Gupta <129592452+Sprint41@users.noreply.github.com>
2023-09-18 16:56:54 +0530

    Add files via upload

---
+++ Day 2/stringslicing.py
@@ -0,0 +1,6 @@
+#Given a string, print its first three characters and last three characters.
+a = str("experiment")
+firsthree_str= a[0:3]
+lasthree_str= a[-3:]
+print("The first three characters of str:",firsthree_str)
+print("The last three characters of str:",lasthree_str)
