6c08ae4f75fc87b96ac3e98355e01ce776be233f
Aditya Gupta <129592452+Sprint41@users.noreply.github.com>
2023-10-04 18:39:29 +0530

    Add files via upload

---
+++ Day 5/10listoflists.py
@@ -0,0 +1,4 @@
+# Create a list of lists representing a 2D matrix and access a particular element within it using indexing.
+list_of_lists=[["Lenovo","Asus","Acer"],["HP","Realme","Oneplus"],["Nokia","Samsung","Apple"]]
+access_elements= list_of_lists[2][2]
+print(access_elements)
