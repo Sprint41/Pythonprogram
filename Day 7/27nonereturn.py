8da4f7027ce055f2a96461ddf2cc0abf72bf7cb1
Aditya Gupta <129592452+Sprint41@users.noreply.github.com>
2023-11-01 02:49:29 +0530

    Add files via upload

---
+++ Day 7/27nonereturn.py
@@ -0,0 +1,7 @@
+#Define a function that doesn't have a return statement and print its return value.
+def no_return():
+
+    print("This function doesn't return anything.")
+result = no_return()
+print("Return value:", result)
+
